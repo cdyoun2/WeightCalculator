6456f32963d5e19816e512201be5179aa8f5f5b6
cdyoun2 <firefast17@gmail.com>
2022-11-02 16:19:21 -0500

    yee

--- items/models.py
+++ items/models.py
@@ -1,7 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Item(models.Model):
     text = models.CharField(max_length=2000)
+
+
+class WorkoutEntry(models.Model):
+    deadlift = []
+    squat = []
+    bench = []
+    clean = []
+    snatch = []
+    ohp = []
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
