0b4fa84790a4e6054b964050208f79a0b9aaa145
cdyoun2 <firefast17@gmail.com>
2022-11-17 16:22:20 -0600

    new html working

--- items/views.py
+++ items/views.py
@@ -3,12 +3,13 @@ from django.views import generic
 from items.models import Item
 from django.http import HttpResponse
 from .models import WorkoutEntry
+#from items.templates import WeightCalculator
 
 # Create your views here.
 
 
 class ItemList(generic.ListView):
-    template_name = 'items/itemlist.html'
+    template_name = 'items/WeightCalculator.html'
     context_object_name = 'item_list'
     model = Item
 
@@ -18,11 +19,12 @@ class ItemList(generic.ListView):
 
 
 class WorkoutEntries (generic.ListView):
-    template_name = 'items/workout.html'
+    template_name = 'items/WeightCalculator.html'
     model = WorkoutEntry
     fields = ['deadlift', 'squat', 'bench', 'clean', 'snatch', 'ohp']
+
     def displayWorkout(request):
-        return render(request, 'workout.html')
+        return render(request, 'WeightCalculator.html')
 
     def post(self, request, *args, **kwargs):
         WorkoutEntries.objects.create(text=request.POST["text"])
