a0ce3f75969f4f4f3eb35aea2fcab4a5332dc027
cdyoun2 <firefast17@gmail.com>
2022-12-08 00:17:54 -0600

    plot displaying data now, just need to correct said data

--- items/processWorkout.py
+++ items/processWorkout.py
@@ -34,51 +34,38 @@ class ProcessWorkout():
                     if (not is_weight_flag and letter != 'x' and letter != ','):
                         string_of_reps += letter
                     if letter == ',':
-                        print('testing reps, weight'+string_of_reps,
-                              " | ", string_of_weight)
-                        deadlift.append([
-                            int(string_of_weight), int(string_of_reps)])
+                        # reset the weight and reps strings
                         string_of_weight = ''
                         string_of_reps = ''
                         count_inner += 1
                     print(letter)
                 count = count + 1
+                print('testing reps, weight'+string_of_reps,
+                      " | ", string_of_weight)
                 deadlift.append([
                     int(string_of_weight), int(string_of_reps)])
-        for i in WorkoutEntry.objects.all():
-            if (count == 0):
-                for letter in i.bench:
-                    if letter == 'x':
-                        is_weight_flag = False
-                    if letter == ',':
-                        is_weight_flag = True
-                    if is_weight_flag and letter != 'x' and letter != ',':
-                        string_of_weight += letter
-                    if (not is_weight_flag and letter != 'x' and letter != ','):
-                        string_of_reps += letter
-                    if letter == ',':
-                        print('testing reps, weight'+string_of_reps,
-                              " | ", string_of_weight)
-                        bench.append([
-                            int(string_of_weight), int(string_of_reps)])
-                        string_of_weight = ''
-                        string_of_reps = ''
-                        count_inner += 1
-                    print(letter)
-                count = count + 1
-                bench.append([
-                    int(string_of_weight), int(string_of_reps)])
+            count = 0
+            is_weight_flag = True
+            string_of_weight = ''
+            string_of_reps = ''
+
+        print(i.date)
         dates.append(i.date)
 
-        dates2 = []
-        for i in range(30):
-            dates2.append(datetime.date.today() - datetime.timedelta(days=i))
+        # create the deadliftTonnage list
+        deadliftTonnage = []
+        for i in deadlift:
+            deadliftTonnage.append(i[0]*i[1])
 
-        # create a list of values
-        values = np.random.randint(0, 100, 30)
+        deadliftTonnage.append(1500)
+        dates.append(datetime.date(2020, 1, 1))
+        dates.append(datetime.date(2020, 1, 2))
+
+        print(deadliftTonnage)
+        print(dates)
 
         # plot the values
-        plt.plot(dates, deadlift)
+        plt.plot(dates, deadliftTonnage)
 
         # set the x-axis to the dates
         plt.gcf().autofmt_xdate()
