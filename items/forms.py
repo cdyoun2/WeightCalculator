b6564d3950ee1e64e1c35b9039e930ee8004a5c5
snahuy <sna.huy0904@gmail.com>
2022-12-12 21:25:07 -0600

    Workout Page Front End Done

--- items/forms.py
+++ items/forms.py
@@ -2,12 +2,18 @@ from django import forms
 
 
 class CreateWorkout(forms.Form):
-    deadlift = forms.CharField(label="deadlift", max_length=200)
-    squat = forms.CharField(label='squat', max_length=200)
-    bench = forms.CharField(label='bench', max_length=200)
-    clean = forms.CharField(label='clean', max_length=200)
-    snatch = forms.CharField(label='snatch', max_length=200)
-    ohp = forms.CharField(label='ohp', max_length=200)
+    deadlift = forms.CharField(label="Deadlift", max_length=200,
+                               widget=forms.TextInput(attrs={'placeholder': 'Enter your Deadlift'}))
+    squat = forms.CharField(label='Squat', max_length=200,
+                            widget=forms.TextInput(attrs={'placeholder': 'Enter your Squat'}))
+    bench = forms.CharField(label='Bench Press', max_length=200,
+                            widget=forms.TextInput(attrs={'placeholder': 'Enter your Bench Press'}))
+    clean = forms.CharField(label='Clean', max_length=200,
+                            widget=forms.TextInput(attrs={'placeholder': 'Enter your Clean'}))
+    snatch = forms.CharField(label='Snatch', max_length=200,
+                             widget=forms.TextInput(attrs={'placeholder': 'Enter your Snatch'}))
+    ohp = forms.CharField(label='Overhead Press', max_length=200,
+                          widget=forms.TextInput(attrs={'placeholder': 'Enter your Overhead Press'}))
 
 
 class LoginForm(forms.Form):
